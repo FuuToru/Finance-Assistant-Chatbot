d2805c7b0618723b10f57de1813dfb0c3437a3e9
Tri <huutri231103@gmail.com>
2024-05-01 18:14:50 +0700

    fetch api

--- src/main.py
+++ src/main.py
@@ -60,11 +60,169 @@ functions = [
             'properties':{
                 'ticker': {
                     'type': 'string',
-                    'description': 'The stock ticker symbol for a compay (for example AAPL for Apple).',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
                 }
             },
-            'required': ['ticker', 'window'],
-        },
+            'required': ['ticker'],
+        }
         
-    }
+    },
+    {
+        'name': 'calculate_SMA',
+        'description': 'Calculate the simple moving average for a given stock ticker and a window.',
+        'parameters': {
+            'type': 'object',
+            'properties':{
+                'ticker': {
+                    'type': 'string',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
+                },
+                'window': {
+                    'type': 'integer',
+                    'description': 'The timeframe to consider when calculating the SMA.',
+                }
+
+            },
+            'required': ['ticker', 'window'],
+        }
+        
+    },
+        {
+        'name': 'calculate_EMA',
+        'description': 'Calculate the exponential moving average for a given stock ticker and a window.',
+        'parameters': {
+            'type': 'object',
+            'properties':{
+                'ticker': {
+                    'type': 'string',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
+                },
+                'window':{
+                    'type': 'integer',
+                    'description': 'The timeframe to consider when calculating the EMA.',
+                }
+            },
+            'required': ['ticker','window'],
+        }
+        
+    },
+    {
+        'name': 'calculate_RSI',
+        'description': 'Calculate the RSI for a given stock ticker.',
+        'parameters': {
+            'type': 'object',
+            'properties':{
+                'ticker': {
+                    'type': 'string',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
+                }
+            },
+            'required': ['ticker'],
+        }
+        
+    },
+    {
+        'name': 'calculate_MACD',
+        'description': 'Calculate the MACD for a given stock ticker.',
+        'parameters': {
+            'type': 'object',
+            'properties':{
+                'ticker': {
+                    'type': 'string',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
+                }
+            },
+            'required': ['ticker'],
+        }
+        
+    },
+        {
+        'name': 'plot_stock_price',
+        'description': 'Plot the stock price for the last year given the ticker symbol of a company.',
+        'parameters': {
+            'type': 'object',
+            'properties':{
+                'ticker': {
+                    'type': 'string',
+                    'description': 'The stock ticker symbol for a company (for example AAPL for Apple).',
+                }
+            },
+            'required': ['ticker'],
+        }
+        
+    },
+
 ]
+
+available_fuctions = {
+    'get_stock_price': get_stock_price,
+    'calculate_SMA': calculate_SMA,
+    'calculate_EMA': calculate_EMA,
+    'calculate_RSI': calculate_RSI,
+    'calculate_MACD': calculate_MACD,
+    'plot_stock_price': plot_stock_price,
+}
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] =[]
+
+st.title('Stock Analysis Chatbot Assistant')
+
+user_input = st.text_input('Your input:')
+
+if user_input:
+    try:
+        st.session_state['messages'].append({'role': 'user','content':f'{user_input}'})
+
+        response = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo-0613',
+            messages = st.session_state['messages'],
+            functions = functions,
+            function_call = 'auto'
+        )
+
+        response_message = response['choices'][0]['message']
+
+        if response_message.get('function_call'):
+            function_name = response_message['function_call']['name']
+            fuction_args = json.loads(response_message['function_call']['arguments'])
+            if function_name in ['get_stock_price','calculate_RSI','calculate_MACD','plot_stock_price']:
+                args_dict = {'ticker': function_args.get('ticker')}
+
+            elif function_name in ['calculate_SMA','calculate_EMA']:
+                args_dict = {'ticker': function_args.get('ticker'),'window': function_args.get('window')}
+
+            function_to_call = available_fuctions[function_name]
+            function_response = function_to_call.get(**args_dict)
+
+            if function_name == 'plot_stock_price':
+                st.image('stock.png')
+            else:
+                st.session_state['messages'].append(response_message)
+                st.session_state['messages'].append({
+                    'role':'function',
+                    'name': function_name,
+                    'content': function_response
+                })
+                second_response = openai.ChatCompletion.create(
+                    model = 'gpt-3.5-turbo-0613',
+                    messages = st.session_state['messages']
+                )
+                st.text(second_response['choices'][0]['message']['content'])
+                st.session_state['messages'].append({
+                    'role':'assistant',
+                    'content': second_response['choices'][0]['message']['content']
+                })
+        else:
+            st.text(response_message)['content']
+            st.session_state['messages'].append({
+                'role':'assistant',
+                'content': response_message['content']
+
+            })
+    except Exception as e:
+        raise e
+
+                
+
+
